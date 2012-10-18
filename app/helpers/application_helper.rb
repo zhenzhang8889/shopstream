9fb8ff8f0339abc8f384c0ec50386b924a973a8b
Gosha Arinich <me@goshakkk.name>
2012-10-18 19:43:16 +0300

    flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class(type)
+    case type
+    when :notice then nil
+    when :error then 'alert'
+    when :alert then 'alert'
+    else type
+    end
+  end
 end

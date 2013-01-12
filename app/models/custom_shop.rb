7cfbd49644e83d79feb6442d3eb059eb7425f135
Gosha Arinich <me@goshakkk.name>
2013-01-12 19:10:42 +0300

    send instructions email properly to self, closes #109

--- app/models/custom_shop.rb
+++ app/models/custom_shop.rb
@@ -2,6 +2,6 @@ class CustomShop < Shop
   after_create :email_instructions
 
   def email_instructions
-    InstructionsMailer.instructions(self, user).deliver
+    InstructionsMailer.instructions(self, user.email).deliver
   end
 end

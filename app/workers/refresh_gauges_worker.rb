e4ace16a7202b4edf6e4a21cb406c03d5ef3f920
Gosha Arinich <me@goshakkk.name>
2013-02-17 13:38:38 +0300

    10 minute refresher expiration

--- app/workers/refresh_gauges_worker.rb
+++ app/workers/refresh_gauges_worker.rb
@@ -1,6 +1,6 @@
 class RefreshGaugesWorker
   include Sidekiq::Worker
-  sidekiq_options queue: :low, unique: true, expiration: 60
+  sidekiq_options queue: :low, unique: true, expiration: 10 * 60
 
   def perform(shop_id)
     shop = Shop.find(shop_id)
